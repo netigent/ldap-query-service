2ab4b4fd35bffd9800d8ae032c7c2f7e2250a247
Karl Cinnamond <kcinnamond@enstoa.com>
2021-09-17 13:57:46 +0100

    Commenting code

--- Netigent.Utils.Ldap/ILdapQueryService.cs
+++ Netigent.Utils.Ldap/ILdapQueryService.cs
@@ -6,22 +6,26 @@ namespace Netigent.Utils.Ldap
 {
 	public interface ILdapQueryService
 	{
+		//Login flag
 		bool LoggedIn { get; }
 
+		//Login Operation
 		bool Login(string domain, string username, string password, out string errorMessage);
 
+		//Users
 		LdapUser GetUser();
-
 		List<LdapUser> GetUsers();
 		LdapUser GetUser(LdapQueryAttribute userQueryType, string userString);
 
+		//Groups
         List<LdapGroup> GetGroups();
 		LdapGroup GetGroup(LdapQueryAttribute groupQueryType, string groupString);
 
+		//Memebership
 		bool MemberOf(LdapQueryAttribute groupQueryType, string groupString);
 		bool MemberOf(LdapQueryAttribute userQueryType, string userString, LdapQueryAttribute groupQueryType, string groupString);
 
+		//Generic
 		List<LdapGeneric> RunQuery(string filter);
-
 	}
 }
